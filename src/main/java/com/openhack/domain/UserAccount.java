4c8f610b96c52b6c6c692c0152843c8506253df5
vishumanvi <vishwanath.manvi@sjsu.edu>
2019-04-22 17:17:45 -0700

    adding UserAccount entity

---
+++ src/main/java/com/openhack/domain/UserAccount.java
@@ -0,0 +1,74 @@
+package com.openhack.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "USERACCOUNT")
+public class UserAccount {
+
+	/** The username. */
+	@Id
+	/** The organization. */
+	@OneToOne(targetEntity=UserProfile.class)
+	@JoinColumn(name = "USERNAME",referencedColumnName="SCREENNAME")
+	private UserProfile user;
+
+	/** The password. */
+	@Column(name = "PASSWORD")
+	private String password;
+	
+	/** The attempts */
+	@Column(name = "ATTEMPTS")
+	private int attempts;
+		
+	/** The status */
+	@Column(name = "STATUS")
+	private String status;
+
+	public UserProfile getUser() {
+		return user;
+	}
+
+	public void setUser(UserProfile user) {
+		this.user = user;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public int getAttempts() {
+		return attempts;
+	}
+
+	public void setAttempts(int attempts) {
+		this.attempts = attempts;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public UserAccount(UserProfile user, String password, int attempts, String status) {
+		super();
+		this.user = user;
+		this.password = password;
+		this.attempts = attempts;
+		this.status = status;
+	}
+
+}
