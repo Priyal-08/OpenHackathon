f3d2c8feed6f3f706bdcf0435188605dedd8e43e
Janhavi Dahihande <janhavidahihande@gmail.com>
2019-05-03 13:08:44 -0700

    Add email sending service

--- src/main/java/com/openhack/service/UserService.java
+++ src/main/java/com/openhack/service/UserService.java
@@ -20,6 +20,10 @@ import com.openhack.exception.DuplicateException;
 
 @Service
 public class UserService {
+	
+	@Autowired
+	private EmailService emailService;
+	
 	/** The user dao. */
 	@Autowired
 	private UserDao userDao;
@@ -34,29 +38,36 @@ public class UserService {
 	
 	@Transactional
 	public ResponseEntity<?>  signupUser(String firstname, String lastname, String email, String password) throws Exception {
-
-		UserProfile userProfile=null;
-		UserAccount userAccount=null;
-
-		if (!isEmailValid(email)) { // if email is invalid 
-			errorResponse = new ErrorResponse("BadRequest", "400", "Invalid e-mail address");
+		try {
+			UserProfile userProfile=null;
+			UserAccount userAccount=null;
+	
+			if (!isEmailValid(email)) { // if email is invalid 
+				errorResponse = new ErrorResponse("BadRequest", "400", "Invalid e-mail address");
+				return ResponseEntity.badRequest().contentType(MediaType.APPLICATION_JSON).body(errorResponse);
+			}		
+			// Event name should be unique so if an event with given name already exist, return BadRequest.
+			userProfile = userDao.findByEmail(email);
+			
+			if (userProfile != null)
+				throw new DuplicateException("UserProfile", "email", email);
+			
+			userProfile = new UserProfile(firstname, lastname, email);
+			//userAccount = new UserAccount(userProfile,password,0,"Pending Verification");
+			
+			userProfile = userDao.store(userProfile);
+			//userAccount = userDao.store(userAccount);
+			String subject = String.format("Open Hackathon Account Verification");
+			String text = String.format("Please confirm your registration for hackathon by following the link below. \n %s", 
+					"Dummy link");
+			emailService.sendSimpleMessage(userProfile.getEmail(), subject , text);
+			response = new UserResponse(userProfile.getEmail());					
+			return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(response);
+		}
+		catch(Exception e) {
+			errorResponse = new ErrorResponse("BadRequest", "400", e.getMessage());
 			return ResponseEntity.badRequest().contentType(MediaType.APPLICATION_JSON).body(errorResponse);
-		}		
-		// Event name should be unique so if an event with given name already exist, return BadRequest.
-		userProfile = userDao.findByEmail(email);
-		
-		if (userProfile != null)
-			throw new DuplicateException("UserProfile", "email", email);
-
-		userProfile = new UserProfile(firstname, lastname, email);
-		userAccount = new UserAccount(userProfile,password,0,"Pending Verification");
-		
-		userProfile = userDao.store(userProfile);
-		//userAccount = userDao.store(userAccount);
-
-		response = new UserResponse(userProfile.getEmail());					
-		return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body(response);
-		
+		}
 	}
 		
 		public static boolean isEmailValid(String email) 
